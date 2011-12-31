c0618367ba839e2cf056326ea41d24d235c580a6
edward sharp <edward@edwardsharp.net>
2011-12-31 07:16:43 -0800

    refactor broken model method into broken rake task

--- micronemez_railz3/app/models/kaltura_video.rb
+++ micronemez_railz3/app/models/kaltura_video.rb
@@ -1,19 +1,4 @@
 class KalturaVideo < ActiveRecord::Base
 
-	public 
-
-	def getContentFromAPI
-	
-		entries = Kaltura::MediaEntry.list
-	
-		#TODO: check to see if record already exists... 
-		# need to account for single changes in each key value...
-		entries.each do |a|
-    	kaltura_video = KalturaVideo.new
-   		kaltura_video.update_attributes a
-  		kaltura_video.save
-		end
-	
-	end
 
 end
